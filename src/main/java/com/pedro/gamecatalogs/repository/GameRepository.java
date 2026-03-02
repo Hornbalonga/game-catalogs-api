f7348d336fad120ce3e1f8f3a85cbb03f898b3e1
Pedro Corrêa <112213077+Hornbalonga@users.noreply.github.com>
2026-03-01 22:15:52 -0300

    feat: add game repository structure

--- src/main/java/com/pedro/gamecatalogs/repository/GameRepository.java
+++ src/main/java/com/pedro/gamecatalogs/repository/GameRepository.java
@@ -3,5 +3,5 @@ package com.pedro.gamecatalogs.repository;
 import com.pedro.gamecatalogs.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class GameRepository  extends JpaRepository<Game, Integer> {
+public class GameRepository  extends JpaRepository<Game, Long> {
 }
